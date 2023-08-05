77ad0f7a35a4f8c92a8e298f0a03a20bdd83db14
Sarah Bateman <sarahb3865@gmail.com>
2023-08-05 12:01:13 +1200

    Added files for Vercel deployment

--- app.js
+++ app.js
@@ -26,3 +26,15 @@ app.use('/api/books', books);
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
